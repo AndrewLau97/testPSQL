e48ad27b44958d8cb15ef011a372dc921e8121ad
Chris Fagan <cfagan88@hotmail.co.uk>
2025-01-09 14:45:09 +0000

    adding supabase setup

--- connection.js
+++ connection.js
@@ -1,4 +1,24 @@
-const {Pool}=require('pg')
-const pool= new Pool()
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-module.exports=pool
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+// const {Pool}=require('pg')
+// const pool= new Pool()
+
+// module.exports=pool
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
